4bc622a4efc67b99b2da784d0715e42fddb50ace
Jacob Pawlik <jacob.pawlik@scopevisio.com>
2023-01-01 13:54:23 +0100

    contract testing

---
+++ backend/plugins/cookie.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+
+/**
+ * This plugins adds some utilities to handle http errors
+ *
+ * @see https://github.com/fastify/fastify-sensible
+ */
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require("@fastify/cookie"), {
+    secret: "my-secret", // for cookies signature
+    hook: "onRequest", // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+    parseOptions: {}, // options for parsing cookies
+  });
+});
